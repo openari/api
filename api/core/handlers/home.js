3f9439e0e88788839872b500c9301d6689cea7a8
Jack Tsai <jacksctsai@gmail.com>
2019-03-28 19:07:40 +0800

    update default msg

--- api/core/handlers/home.js
+++ api/core/handlers/home.js
@@ -2,7 +2,7 @@ const pjson = require('../../../package.json');
 
 module.exports.hello = {
   handler: function (request, h) { // eslint-disable-line no-unused-vars
-    var msg = 'OpenaRI API server (v.' + pjson.version + ') is up and running.';
+    var msg = 'OpenARI API server (v.' + pjson.version + ') is up and running.';
     return { msg: msg };
   }
 };
