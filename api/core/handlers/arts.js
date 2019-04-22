907d75113febc3255e9a90d3769477f1e0a53e9d
Jack Tsai <jacksctsai@gmail.com>
2019-04-23 04:25:48 +0800

    allow price to be null

--- api/core/handlers/arts.js
+++ api/core/handlers/arts.js
@@ -37,7 +37,7 @@ const createBodySchema = Joi.object({
     email: Joi.string().allow(''),
     phone: Joi.string().allow(''),
     contact_public: Joi.boolean().required(),
-    price: Joi.string().allow(''),
+    price: Joi.string().allow('').allow(null),
     price_public: Joi.boolean().required(),
   })
 });
